036247aa0abf641cf879f1feac94b5a4e4960b88
yuka <yuaaasly116@gmail.com>
2022-02-03 05:26:05 +0000

    [add]詳細画面

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -16,15 +16,19 @@ class PostImagesController < ApplicationController
   end
   
   def show
+    @post_image = PostImage.find(params[:id])
   end
   
   def destroy
+    @post_image = PostImage.find(params[:id])
+    @post_image.destroy
+    redirect_to post_images_path
   end
   
   private
   
   def post_image_params
-    params.require(:post_image).permit(:shop_name, :mage, :caption)
+    params.require(:post_image).permit(:shop_name, :image, :caption)
   end
   
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'd420eea65b389a1092154d5d7422f37e0e9ab803ccd653ddf30402791330af8c0c1f9516e4902989e16ec6615cf50afec20d7a6a3ab3655e57b04df13678c5d6'
